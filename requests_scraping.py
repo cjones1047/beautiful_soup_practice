dd02d9cd1cd68e96af554d87881117d827f6dc7a
cjones1047 <cjmj1047@gmail.com>
2022-09-29 13:05:59 -0500

    requests_scraping.py now gets current stock price by appropriate HTML attribute

--- requests_scraping.py
+++ requests_scraping.py
@@ -1,8 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
+import re #imports regular expression in Python
 
-url = "https://www.investopedia.com/financial-edge/0911/top-6-websites-for-finding-financial-stats.aspx"
+url = "https://seekingalpha.com/symbol/MU"
 
-result = requests.get(url)
+result = requests.get(url).text
 
-print(result.text)
+doc = BeautifulSoup(result, 'html.parser')
+
+current_price = doc.find('span',{'data-test-id':'symbol-price'})
+print(current_price.contents)
+
+#<span jsname="vWLAgc" class="IsqQVc NprOob wT3VGc">49.88</span>
+
+# <div id="sdp-price-chart-price-change" class="css-gg4vpm"><div><div class="css-1tqud6q"><span class="css-hw40uo">-$1.37 (-2.69%)</span><span class="css-o58iq6">Today</span></div><div class="css-1tqud6q"></div></div></div>
+
+#<span class="ktG blcD buK bu1 bu6" data-test-id="symbol-price">$<!-- -->49.41</span>
